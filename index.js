db38f0719d17598141865365c64a6f5213daac2d
Charles Howard <cdhoward@add123.com>
2018-01-18 11:00:33 -0500

    init

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import reactDom from 'react-dom'
+
+const Example = () => <h1>howdy!</h1>
+
+reactDom.render(<Example />, document.getElementById('app'))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Bare Bones Redux
+
+A minimal React Redux template for building example projects.
